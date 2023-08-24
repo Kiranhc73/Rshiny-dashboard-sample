3bb1a2c93f8cf08217d77145b1b6615dc4ffb3c4
Kiran H C <134852180+Kiranhc73@users.noreply.github.com>
2023-08-24 16:09:30 +0530

    major id: Changes

--- Shiny.R
+++ Shiny.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(shiny)
 library(plotly)
+library(scales)
 library(readxl)
 
 
@@ -15,18 +16,17 @@ store_data %>% colnames()
 col_Sub_profit<- function(){
   store_data %>% 
     group_by(`Sub-Category`) %>% 
-    summarise(Profit=sum(Profit), .groups = "drop" ) %>% 
-    mutate(Profit=Profit %>% round(2) %>% sort(decreasing = T)) %>% 
-    ggplot()+
-    geom_col(aes(x=`Sub-Category`,y=Profit,fill=`Sub-Category`),show.legend = FALSE)+
-    geom_hline(yintercept = 0, color = "red", linetype = "dashed")+
-    theme_minimal()+
-    theme(axis.text.x = element_text(angle = 45))+
-    scale_y_continuous(labels = scales::dollar_format(prefix = "", suffix = "k", scale = 1e-3))-> bar_chart
-  ggplotly(bar_chart) %>% 
-    layout(showlegend = FALSE)
-    }
-
+    summarise(Profit = sum(Profit), .groups = "drop") %>% 
+    mutate(Profit = Profit %>% round(2) %>% sort(decreasing = TRUE)) %>% 
+    ggplot() +
+    geom_col(aes(x = `Sub-Category`, y = Profit, fill = `Sub-Category`), show.legend = FALSE) +
+    geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45)) +
+    scale_y_continuous(labels = scales::dollar_format(prefix = "", suffix = "k", scale = 1e-3)) 
+    # geom_text(aes(x = `Sub-Category`, y = Profit, label = scales::dollar(Profit, prefix = "", suffix = "k", scale = 1e-3)),
+              # hjust=1, angle = 90, size = 4, vjust = -0.5)
+}
 
 col_sub_sales<-function(){
   store_data %>% 
@@ -39,9 +39,43 @@ col_sub_sales<-function(){
     theme_minimal()+
     theme(axis.text.x = element_text(angle = 45),
           axis.text.y = element_text()) +
-    scale_y_continuous(labels = scales::dollar_format(prefix = "", suffix = "k", scale = 1e-3))-> bar_chart
-  ggplotly(bar_chart) %>% 
-    layout(showlegend = FALSE)
+    scale_y_continuous(labels = scales::dollar_format(prefix = "", suffix = "k", scale = 1e-3))
+    # geom_text(aes(x=`Sub-Category`,y=Sales,label = scales::dollar(Sales, prefix = "",suffix = "k", scale = 1e-3)), vjust = -0.5,hjust=1, angle = 90,size=4)
+ 
 }
+
+col_region_sales<-function(){
+  store_data %>% 
+    group_by(Region) %>% 
+    summarise(Sales=sum(Sales), .groups = "drop" ) %>% 
+    mutate(Sales=Sales %>% round(2) %>% sort(decreasing = T)) %>% 
+    ggplot()+
+    geom_col(aes(x=Region,y=Sales,fill=Region),show.legend = FALSE)+
+    geom_hline(yintercept = 0, color = "red", linetype = "dashed")+
+    theme_minimal()+
+    theme(axis.text.x = element_text(angle = 45),
+          axis.text.y = element_text()) +
+    scale_y_continuous(labels = scales::dollar_format(prefix = "", suffix = "k", scale = 1e-3))
+    # geom_text(aes(x=Region,y=Sales,label = scales::dollar(Sales, prefix = "",suffix = "k", scale = 1e-3)), vjust = -0.5, hjust=1, angle = 90,size=4)
+
+}
+
+
+col_region_profit<- function(){
+  store_data %>% 
+    group_by(Region) %>% 
+    summarise(Profit=sum(Profit), .groups = "drop" ) %>% 
+    mutate(Profit=Profit %>% round(2) %>% sort(decreasing = T)) %>% 
+    ggplot()+
+    geom_col(aes(x=Region,y=Profit,fill=Region),show.legend = FALSE)+
+    geom_hline(yintercept = 0, color = "red", linetype = "dashed")+
+    theme_minimal()+
+    theme(axis.text.x = element_text(angle = 45))+
+    scale_y_continuous(labels = scales::dollar_format(prefix = "", suffix = "k", scale = 1e-3))
+    # geom_text(aes(x=Region,y=Profit,label = scales::dollar(Profit, prefix = "",suffix = "k", scale = 1e-3)), vjust = -0.5,hjust=1, angle = 90, size = 4)
+
+}
+
+
 
 
--- Shiny.app.R
+++ Shiny.app.R
@@ -6,39 +6,71 @@ ui <- dashboardPage(
   dashboardHeader(title = "Sales Analysis"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Sales vs Sub-cat", tabName = "Sales", icon = icon("dashboard")),
-      menuItem("Profit vs Sub-cat", tabName = "Profit", icon = icon("dashboard"))
+      menuItem("Sales Analysis", tabName = "Sales", icon = icon("dashboard")),
+      menuItem("Profit Analysis", tabName = "Profit", icon = icon("dashboard"))
     )
   ),
   dashboardBody(
     tabItems(
       tabItem(
         tabName = "Sales",
-        fluidRow(
-          column(12,
-            plotlyOutput("sales")
+        wellPanel( fluidRow(
+          box(width=6,height = "100%",
+            
+                 plotOutput("sales"),
+                 title="sales_vs_sub-category"
+                 
+          
+      
+          ), box(width=6,height = "100%",
+                
+                   plotOutput("sales2"),
+                   title="sales_vs_region"
+                 
+                 
           )
         )
-      ),
+       
+      )),
       tabItem(
         tabName = "Profit",
-        fluidRow(
-          column(12,
-            plotlyOutput("profit")
+        wellPanel( fluidRow(
+          box(width=6,height = "100%",
+              
+                plotOutput("profit"),
+                title="profit_vs_sub-category"
+              
+              
+          ), box(width=6,height = "100%",
+                 
+                   plotOutput("profit2"),
+                   title ="profit_vs_region"
+                 
+                 
           )
         )
+        
+        )
+        
       )
     )
   )
 )
 
 server <- function(input, output) {
-  output$sales <- renderPlotly({
+  output$sales <- renderPlot({
     col_sub_sales()
   })
 
-  output$profit <- renderPlotly({
+  output$profit <- renderPlot({
     col_Sub_profit()
+  })
+  output$sales2 <- renderPlot({
+    col_region_sales()
+  })
+  
+  output$profit2 <- renderPlot({
+    col_region_profit()
   })
 }
 
